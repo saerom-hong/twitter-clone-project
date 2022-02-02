ef8f2d814cf4da7e03e6e24ad1884092a9e22161
saerom-hong <saeromhong12@gmail.com>
2022-02-02 21:35:24 +0100

    fix eslint error

--- src/fBase.js
+++ src/fBase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
