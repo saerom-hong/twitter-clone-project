498063c64954a1e63dd4f007dc7fa40883c009b0
saerom-hong <saeromhong12@gmail.com>
2022-02-03 15:17:13 +0100

    fix minor errors

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 
 import { collection, orderBy, query, onSnapshot } from "firebase/firestore";
 
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,6 +1,6 @@
 import { signOut } from "firebase/auth";
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../fBase";
 import { updateProfile } from "firebase/auth";
 
